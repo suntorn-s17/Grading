84b0b26d9ac8e48883fa229d34d2e7b87c60e1f4
suntorn sriket <snsk1701@hotmail.com>
2024-10-01 16:18:20 +0700

    Update

--- Grading/Program.cs
+++ Grading/Program.cs
@@ -28,31 +28,31 @@ namespace Grading
                     {
                         Console.WriteLine("Grade A.");
                     }
-                    else if (score >= 75 || score <= 79)
+                    else if (score >= 75 && score <= 79)
                     {
                         Console.WriteLine("Grade B+.");
                     }
-                    else if (score >= 70 || score <= 74)
+                    else if (score >= 70 && score <= 74)
                     {
                         Console.WriteLine("Grade B.");
                     }
-                    else if (score >= 65 || score <= 69)
+                    else if (score >= 65 && score <= 69)
                     {
                         Console.WriteLine("Grade C+.");
                     }
-                    else if (score >= 60 || score <= 64)
+                    else if (score >= 60 && score <= 64)
                     {
                         Console.WriteLine("Grade C.");
                     }
-                    else if (score >= 55 || score <= 59)
+                    else if (score >= 55 && score <= 59)
                     {
                         Console.WriteLine("Grade D+.");
                     }
-                    else if (score >= 50 || score <= 54)
+                    else if (score >= 50 && score <= 54)
                     {
                         Console.WriteLine("Grade D.");
                     }
-                    else if (score >= 0 || score <= 49)
+                    else if (score >= 0 && score <= 49)
                     {
                         Console.WriteLine("Grade F.");
                     }
